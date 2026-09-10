6d6cf9eb27c1df29c9802ceaa5c1a823e90d3895
João Neto <58696739+joaon200@users.noreply.github.com>
2026-09-10 11:04:53 +0100

    comments on the code

--- Smart Farming/base de dados/bd pit.sql
+++ Smart Farming/base de dados/bd pit.sql
@@ -3,6 +3,7 @@ drop table if exists amostra;
 drop table if exists sensores;
 drop table if exists user1;
 
+--create tables to save permanently data
 create table user1(
 	ID int primary key auto_increment not null,
     nome varchar(30) not null,
@@ -11,11 +12,10 @@ create table user1(
 create table sensores(
 	ID_P int primary key not null,
     ID_USER int not null,
-	foreign key(ID_USER) references user1 (ID) 
+	foreign key(ID_USER) references user1 (ID) --associate an ESP32 with a user
     on update cascade 
     on delete restrict
 );
-
 create table amostra(
 	luz int not null,
     temp float not null,
@@ -23,13 +23,12 @@ create table amostra(
     datahora varchar(19) not null,
     ID_PLACA int not null, 
     primary key (datahora,ID_PLACA),
-    foreign key(ID_PLACA) references sensores(ID_P) 
+    foreign key(ID_PLACA) references sensores(ID_P) --linking the data to an ESP32
     on update cascade 
     on delete restrict
 );
 
-
-
+--insert some data to a database
 insert into  user1 (nome, pass) values("joao", "123joao");
 insert into  sensores values(1,1),(2,1);
 insert into  amostra values(1, 24.5, 60, "2022-05-19/14-47-25",1),(200,25.4, 70, "2022-05-19/14-47-45",2),(1, 24.5, 60, "2022-05-19/14-47-50",2);
